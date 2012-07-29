d91c686ca92203f937c7c4762e05f4aa469bed3e
Monica Sharma <monicasharma1053@gmail.com>
2012-07-29 17:03:11 -0500

    initial setup without test framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatapultCalendar::Application.config.secret_token = 'da4f2c8c048a1b1c8cfec59460a02ee8dd5be140be9f6518f8eaf1c4b74e37af02a854b44e5eb394ddf955893ab3ce87c85dc5ca76d1b39d39c87a6d13de48d6'

0290d9c4f3a6459bb691da1c5faf72a8952d7d2c
Monica Sharma <monicasharma1053@gmail.com>
2012-07-29 17:58:15 -0500

    dynamic titles with helper
    defined full_title method in application_helper
    edited application layout to include full_title
    described h1, base title and custom title in static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+ 
+ describe "Home page" do 
+ 	
+ 	it "should have h1 'Catapult Office Hours'" do
+ 		visit '/static_pages/home'
+ 		page.should have_selector('h1', :text => 'Catapult Office Hours') 		
+ 	end
+
+ 	it "should have the base title 'Catapult Office Hours'" do 
+ 		visit '/static_pages/home'
+ 		page.should have_selector('title', :text => 'Catapult Office Hours')
+    end
+
+    it "should not have a custom page title" do 
+    	visit '/static_pages/home'
+    	page.should_not have_selector('title', :text => '| Home' )
     end
   end
 end

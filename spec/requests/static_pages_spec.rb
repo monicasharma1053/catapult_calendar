d4810061affe8f95782596cc612d2b9ae0531833
Monica Sharma <monicasharma1053@gmail.com>
2012-07-30 16:28:02 -0500

    ugh spacing edits

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "StaticPages" do
 	describe "Home page" do 
 		before { visit root_path } 
 
-		#it { should have_selector('h1', text: 'Catapult Office Hours') }
+		it { should have_selector('h1', text: 'Catapult Office Hours') }
 		it { should have_selector('title', text: full_title('')) }
 		it { should_not have_selector 'title', text: '| Home' }		
 	end

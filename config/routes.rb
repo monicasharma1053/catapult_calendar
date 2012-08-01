d299dc6ea9af695a8f69419a8e6f4ea93a4cc80e
Monica Sharma <monicasharma1053@gmail.com>
2012-08-01 16:20:38 -0500

    sign up integration tests and basic template
    created tests for h1 and title

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ CatapultCalendar::Application.routes.draw do
 
   root to: 'static_pages#home'
 
+  match '/signup', to:'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

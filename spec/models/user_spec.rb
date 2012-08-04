93658225350ce0fc7b1ea53e3204a7d56d4365e4
Monica Sharma <monicasharma1053@gmail.com>
2012-08-04 18:33:46 -0500

    name and email validations
    name and email validations, also tests for name, email when left blank

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,27 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before { @user = User.new(name: "Example", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do 
+  	before { @user.name = " " }
+  	it { should_not be_valid } 	
+  end
+
+  describe "when email address is not present" do
+  	before { @user.email = " " }
+  	it { should_not be_valid }
+  end
+
+
+
+
 end

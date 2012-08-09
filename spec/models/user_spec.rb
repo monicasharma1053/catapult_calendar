34d6aba265efd373de8dd1f3ea8899dafe34f3e8
Monica Sharma <monicasharma1053@gmail.com>
2012-08-09 12:53:42 -0500

    testing password and password confirmation
    added test for blank password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,13 +13,16 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(name: "Example", email: "user@example.com") }
+  before { @user = User.new(name: "Example", email: "user@example.com",
+                            password: "example", password_confirmation: "example") }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -68,6 +71,10 @@ describe User do
   	it { should_not be_valid }
   end
 
+  describe "when password is not present" do 
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
 
 
 end

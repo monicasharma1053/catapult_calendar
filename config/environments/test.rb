2d50709e9a2de4db5b06fbebcbf931025ba50b56
Monica Sharma <monicasharma1053@gmail.com>
2012-08-10 16:30:53 -0500

    redefined bcrypt cost factor
    to add speed

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,10 @@ CatapultCalendar::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering BCrypt's cost function.
+  require 'BCrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end

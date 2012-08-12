f115382e7adc228e5962cc7e7a4ee714c19bf8b9
Monica Sharma <monicasharma1053@gmail.com>
2012-08-12 15:39:15 -0500

    added flash message to user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
+  		flash[:success] = "Welcome to Catapult Office Hours!"
   		redirect_to @user
   	else
   		render 'new'

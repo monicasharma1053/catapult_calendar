402f75d93cf9d3b6726762d4203ea67d226dfdb7
Monica Sharma <monicasharma1053@gmail.com>
2012-08-13 14:54:04 -0500

    update method in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,13 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      #
+    else
+      render 'edit'
+    end
+  end
 end

697a870860d6c4154092df5c3f86a8ce8b987cd3
Monica Sharma <monicasharma1053@gmail.com>
2012-08-12 23:58:55 -0500

    added sign in and sign out routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ CatapultCalendar::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/signup', to:'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

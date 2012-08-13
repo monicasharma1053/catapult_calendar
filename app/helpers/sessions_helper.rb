7c8cf382f11a4a9a94913d2d5854ff49f226c710
Monica Sharma <monicasharma1053@gmail.com>
2012-08-13 16:02:58 -0500

    correct user method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,10 @@ module SessionsHelper
   	@current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def signed_in?
   	!current_user.nil?
   end

5e70d039707a19a55a548493ad1a765e8f7a0e00
Monica Sharma <monicasharma1053@gmail.com>
2012-08-13 15:01:26 -0500

    finished user update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      #
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end

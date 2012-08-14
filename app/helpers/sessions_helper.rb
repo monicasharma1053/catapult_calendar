d83d358b26e1bbc3328baaabe6d4b8be62ae19a4
Monica Sharma <monicasharma1053@gmail.com>
2012-08-13 21:11:37 -0500

    friendly forwarding
    css error on visiting edit page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,4 +26,13 @@ module SessionsHelper
   	cookies.delete(:remember_token)
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  
 end

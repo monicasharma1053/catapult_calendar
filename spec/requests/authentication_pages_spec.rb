1093dd3c1a94efa14132f958bbd84083962d339e
Monica Sharma <monicasharma1053@gmail.com>
2012-08-15 11:56:14 -0500

    users link uri

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,6 +35,8 @@ describe "AuthenticationPages" do
   	  	click_button "Sign in"
   	  end
   	  it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users', href: users_path) }
   	  it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
   	  it { should have_link('Sign out', href: signout_path) }
